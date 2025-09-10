db1a070484dfa2b88a9ab9ca9c16d1588e2b599f
Andreas Ettlinger <andreas.ettlinger@josephinum.at>
2025-09-10 14:25:48 +0200

    initialization of branch with content for github pages

--- mef_agri/models/management/sowing/model_jr.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from ...base import Model
-
-
-class Sowing(Model):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
--- mef_agri/utils/misc.py
+++
@@ -1,5 +0,0 @@
-
-def set_attributes(obj, attr_dict):
-    for key, val in attr_dict.items():
-        if hasattr(obj, key):
-            setattr(obj, key, val)
