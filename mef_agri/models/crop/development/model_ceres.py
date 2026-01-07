e92ec742aa01f1a06cd4ce4adf04fae54d7baf6d
Andreas Ettlinger <andreas.ettlinger@josephinum.at>
2026-01-07 14:58:37 +0100

    first version of ceres-development model

--- mef_agri/models/crop/development/model_ceres.py
+++ mef_agri/models/crop/development/model_ceres.py
@@ -22,7 +22,7 @@ class Development(Model):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # TODO
+        self._zv:np.ndarray = None
 
     @Model.is_quantity(Q.STATE, U.none)
     def growth_stage(self) -> np.ndarray:
@@ -43,6 +43,17 @@ class Development(Model):
         :math:`c_{\textrm{bbch},k}\ [\ ]` - [R11]_ (table 1)
 
         :return: BBCH stages
+        :rtype: numpy.ndarray
+        """
+
+    @Model.is_quantity(Q.ROUT, U.per_day)
+    def growth_stage_rate(self) -> np.ndarray:
+        r"""
+        MQ - Random Output
+
+        :math:`c_{\textrm{D-}\Delta\textrm{gs},k}\ [\frac{1}{day}]`
+
+        :return: daily change/rate of growth stage
         :rtype: numpy.ndarray
         """
 
@@ -130,7 +141,7 @@ class Development(Model):
         r"""
         MQ - Parameter
 
-        :math:`c_{\textrm{D-tt1},0}\ [^\circ C\cdot day]` - [R11]_ (table 1 + 2)
+        :math:`c_{\textrm{D-tt9},0}\ [^\circ C\cdot day]` - [R11]_ (table 1 + 2)
 
         :return: thermal time from sowing to emergence
         :rtype: numpy.ndarray
@@ -141,9 +152,20 @@ class Development(Model):
         r"""
         MQ - Parameter
 
-        :math:`c_{\textrm{D-tt2},0}\ [^\circ C\cdot day]` - [R11]_ (table 1 + 2)
+        :math:`c_{\textrm{D-phy},0}\ [^\circ C\cdot day]` - [R11]_ (table 1 + 2)
 
         :return: phyllochron (also a thermal time)
+        :rtype: numpy.ndarray
+        """
+
+    @Model.is_quantity(Q.PARAM, U.degCday)
+    def tt_filling2maturity(self) -> np.ndarray:
+        r"""
+        MQ - Parameter
+
+        :math:`c_{\textrm{D-tt5},0}\ [^\circ C\cdot day]` - [R11]_ (table 1 + 2)
+
+        :return: thermal time from grain filling to maturity
         :rtype: numpy.ndarray
         """
 
@@ -171,7 +193,7 @@ class Development(Model):
 
     def initialize(self, epoch):
         super().initialize(epoch)
-        # TODO
+        self._zv = np.zeros((self.model_tree.n_particles,))
 
     def update(self, epoch):
         super().update(epoch)
@@ -182,10 +204,63 @@ class Development(Model):
         self.f_photoperiod = 1. - (self.sens_photoperiod / 10000.) * np.power(
             20. - self.dl.value
         )
+        self.cond_fv_fp()
 
-        # TODO implement unmodified equations from [R11]_ (table 1)
+        t_trunc = self.tavg.value - self.temperature_base
+        t_trunc_thresh = np.maximum(self._zv, t_trunc)
+        cgs1 = self.growth_stage < 1.
+        cgs2 = (self.growth_stage >= 1.) & (self.growth_stage < 3.)
+        cgs3 = (self.growth_stage >= 3.) & (self.growth_stage < 4.)
+        cgs4 = (self.growth_stage >= 4.) & (self.growth_stage < 5.)
+        cgs5 = (self.growth_stage >= 5.) & (self.growth_stage < 6.)
+        cgs6 = (self.growth_stage >= 6.) & (self.growth_stage < 7.)
+        cgs7 = (self.growth_stage >= 7.) & (self.growth_stage < 8.)
+        cgs = [cgs1, cgs2, cgs3, cgs4, cgs5, cgs6, cgs7]
+        # growth stage
+        dgs1 = t_trunc_thresh / self.tt_sowing2emergence
+        dgs2 = t_trunc_thresh / ((400. / 95.) * self.phyllochron)
+        dgs2 *= np.minimum(self.f_vernalization, self.f_photoperiod)
+        dgs3 = t_trunc_thresh / (3. * self.phyllochron)
+        dgs4 = t_trunc_thresh / (2. * self.phyllochron)
+        dgs5 = t_trunc_thresh / 200.
+        dgs6 = np.maximum(self._zv, t_trunc - 1.) / (
+            (self.tt_filling2maturity + 21.5) / 0.05
+        )
+        dgs7 = t_trunc_thresh / 250.
+        self.growth_stage_rate = np.select(
+            cgs, [dgs1, dgs2, dgs3, dgs4, dgs5, dgs6, dgs7]
+        )
+        self.growth_stage += self.growth_stage_rate
+
+        # BBCH
+        # NOTE equations are changed compared to [R11]_ table 1 due to strange 
+        # inconsistencies in this paper
+        bbch1 = 5. * self.growth_stage
+        bbch2 = 5. + 0.5 * (self.growth_stage - 1.) * 25.
+        bbch3 = 30. + (self.growth_stage - 3.) * 10.
+        bbch4 = 40. + (self.growth_stage - 4.) * 17.
+        bbch5 = 57. + (self.growth_stage - 5.) * 14.
+        bbch6 = 71. + (self.growth_stage - 6.) * 19.
+        bbch7 = 90. + (self.growth_stage - 7.) * 10.
+        self.bbch = np.select(
+            cgs, [bbch1, bbch2, bbch3, bbch4, bbch5, bbch6, bbch7]
+        )
 
     @Model.is_condition
     def cond_fv_fp(self) -> None:
-        # TODO
-        pass
+        """
+        Ensures, that :func:`f_vernalization` and :func:`f_photoperiod` stay in 
+        the range [0, 1]
+        """
+        self.f_vernalization = np.where(
+            self.f_vernalization >= 0.0, self.f_vernalization, 0.0
+        )
+        self.f_vernalization = np.where(
+            self.f_vernalization <= 1.0, self.f_vernalization, 1.0
+        )
+        self.f_photoperiod = np.where(
+            self.f_photoperiod >= 0.0, self.f_photoperiod, 0.0
+        )
+        self.f_photoperiod = np.where(
+            self.f_photoperiod <= 1.0, self.f_photoperiod, 1.0
+        )
